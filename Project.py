9de1f22bf827787c82482e5a8fa5b3dda303eee6
shl1411 <41832170+shl1411@users.noreply.github.com>
2021-09-30 20:56:21 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,2 +1,36 @@
 #this is the main code for the dna project
-#Names etc..
+#Names: Shaun Lee, __
+dna = 'AAAACCCGGT'
+# s(dna)
+def s(dna):
+    count = {}
+    count['A'] = dna.count('A')
+    count['C'] = dna.count('C')
+    count['G'] = dna.count('G')
+    count['T'] = dna.count('T')
+    return count
+
+#dna2rna
+def dna2rna(dna):
+    rna = ''
+    for symbol in dna:
+        if symbol == 'T':
+            rna = rna + 'U'
+        else:
+            rna = rna + symbol
+    return rna
+#reverse complement
+def reverse_complement(dna):
+    rna = ''
+    dna = dna[::-1]
+    for symbol in dna:
+        if symbol == 'A':
+            rna = rna + 'T'
+        elif symbol == 'C':
+            rna = rna + 'G'
+        elif symbol == 'G':
+            rna = rna + 'C'
+        elif symbol == 'T':
+            rna = rna + 'A'
+    return rna
+print (reverse_complement(dna))

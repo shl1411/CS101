f0495f26e11077c1c441e0cbb44ec6b05faaf3f9
JBG5 <92012267+JBG5@users.noreply.github.com>
2021-10-06 23:37:54 -0500

    Update Project.py
    The first is untested, the other two have been tested and debugged

--- Project.py
+++ Project.py
@@ -33,4 +33,50 @@ def reverse_complement(dna):
         elif symbol == 'T':
             rna = rna + 'A'
     return rna
+
+#======================================================
+#beginning of my work
+#======================================================
+
+#This is untested
+
+def splice_rna(dna, intron_list):   #splits dna into exons, removing the introns. then translates in into rna, and then into a protein string
+    for intron in intron_list:
+        dna.replace(intron,"#")
+    exons=dna.split("#")
+    protein=[]
+    for exon in exons:
+        exon_rna= dna2rna(exon)  #do I need to invert it also? It wasn't clear and I can't test it
+        protein.append(rna2codon(exon_rna))
+    protein_str= protein.join()
+    return protein_str
 print (reverse_complement(dna))
+
+
+#returns the number of possible source rna strings that could make a protein mod 1,000,000
+def source_rna(protein):
+    amino_freq={'F': 2, 'L': 6, 'I': 3, 'M': 1, 'V': 4, 'S': 6, 'P': 4, 'T': 4, 'A': 4, 'Y': 2, 'Stop': 3, 'H': 2, 'Q': 2, 'N': 2, 'K': 2, 'D': 2, 'E': 2, 'C': 2, 'W': 1, 'R': 6, 'G': 4}
+    protein = protein.upper()
+    possible_sorces=3 #there are three possible stops
+    for i in protein:
+        possible_sorces = possible_sorces*amino_freq[i] #multiply by number of possible amino acids
+    return possible_sorces%1000000
+
+
+#returns the average number of offspring displaying the dominant trait given a list of integers representing types of pairs. 
+#AA-AA
+#AA-Aa
+#AA-aa
+#Aa-Aa
+#Aa-aa
+#aa-aa
+
+def count_dom_phenotype(genotypes):
+    if len(genotypes) != 6:
+        print("nope")
+        exit()
+    dom_chance=[1, 1, 1, 0.75, 0.5, 0] #chance any one child displays the dominant trait
+    dom_kids=0
+    for i in range(6):
+        dom_kids+= 2*dom_chance[i]*genotypes[i] #two children per pair, multiplied by the No. of pairs
+    return dom_kids

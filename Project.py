ad1d8c63f2ca870569b66e059c7d86d00fcb8d4d
shl1411 <41832170+shl1411@users.noreply.github.com>
2021-10-06 21:44:55 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -34,3 +34,40 @@ def reverse_complement(dna):
             rna = rna + 'A'
     return rna
 print (reverse_complement(dna))
+
+#rna2codon
+def rna2codon(triplet):
+    genetic_code = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    }
+    allowed_codons = set('ACGU')
+    if triplet in genetic_code:
+        return (genetic_code[triplet])
+    else:
+        return ("Invalid")
+
+def locate_substring(dna_snippet, dna):
+    indexes=[]
+    for i in range(len(dna_snippet)):
+        if dna_snippet.startswith(dna, i):
+            indexes.append(i)
+    print(indexes)
+    
+def  hamming_dist(dna1, dna2):
+    hammingdistance = 0
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            hammingdistance += 1
+        else:
+            continue
+    return hammingdistance

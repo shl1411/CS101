94e9043f2fdc14508db425f5ed7ee025a52c1543
shl1411 <41832170+shl1411@users.noreply.github.com>
2021-10-24 00:52:13 -0500

    Update Milestone2.py

--- Milestone2.py
+++ Milestone2.py
@@ -9,6 +9,9 @@ def find_splice(dna_motif, dna):
                 count = dna.index(i) + count+1
                 dna = dna[dna.index(i)+1:]
                 break
+            else:
+                position = []
+                break
     return position
 
 print (find_splice(dna_motif,dna))
